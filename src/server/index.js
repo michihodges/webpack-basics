226b156192241fd2519c381f816e7b315d85e74c
michihodges <michihodges@gmail.com>
2022-07-26 23:51:53 +0200

    ref: Server setup
    Added further comments.

--- src/server/index.js
+++ src/server/index.js
@@ -5,12 +5,12 @@ const express = require('express')
 // Start up an instance of app
 const app = express()
 
-
+// Initialize the main project folder
 app.use(express.static('src/client'))
 console.log(__dirname)
 
+// Setup server
 const port = 3000
-
 app.listen(port, ()=>{
     console.log(`server running on localhost ${port}`)
 })
