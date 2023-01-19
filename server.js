6931e14b1f3fbb55057faba1a91893d590c1c8a9
michihodges <michihodges@gmail.com>
2023-01-19 23:32:17 +0100

    feat: Express server

---
+++ server.js
@@ -0,0 +1,14 @@
+// Require Express to run server and routes
+const express = require('express');
+
+// Require Body-Parser and CORS
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
