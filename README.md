6564961e061247b5e70e4922379e3590d3b13f0e
michihodges <michihodges@gmail.com>
2023-01-23 20:25:05 +0100

    feat: Express

--- README.md
+++ README.md
@@ -11,5 +11,31 @@ Create a `server.js` file:
 touch server.js
 ```
 
+Add the following code:
+```js
+// Empty object to act as endpoint for all routes
+projectData = {};
+
+// Express
+const express = require('express');
+const app = express();
+
+// Body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize main project folder
+app.use(express.static('website'));
+
+// Local server
+const port = 8000;
+const server = app.listen(port, ()=>{console.log(`server running on localhost ${port}`)});
+```
+
 ## Return to Main
 [Return to Main](https://github.com/michihodges/webpack-basics)
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-// Require Express to run server and routes
+// Empty object to act as endpoint for all routes
+projectData = {};
+
+// Express
 const express = require('express');
-
-// Require Body-Parser and CORS
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-// Start up an instance of app
 const app = express();
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+// Body parser
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize main project folder
+app.use(express.static('website'));
+
+// Local server
+const port = 8000;
+const server = app.listen(port, ()=>{console.log(`server running on localhost ${port}`)});
