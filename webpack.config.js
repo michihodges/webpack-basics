f39f62e7dbc52f2ca495a40092966ad8dcc85ba5
michihodges <michihodges@gmail.com>
2023-03-14 16:46:33 +0100

    feat: Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,14 @@
 const path = require("path")
 const webpack = require("webpack")
 module.exports = {
+    entry: './src/client/index.js', // Make sure to add a comma before the next line of code
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader" // The last line does not need a comma
+            }
+        ]
+    } // The last line does not need a comma
 }
