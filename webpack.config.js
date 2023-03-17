3737b7060d849c50da1709a021cbbfe36e8a661a
michihodges <michihodges@gmail.com>
2023-03-17 11:28:11 +0100

    feat: Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 const path = require("path")
 const webpack = require("webpack")
+const htmlWebpackPlugin = require("html-webpack-plugin")
 module.exports = {
-    entry: './src/client/index.js', // Make sure to add a comma before the next line of code
+    entry: './src/client/index.js',
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "babel-loader" // The last line does not need a comma
+                loader: "babel-loader"
             }
         ]
-    } // The last line does not need a comma
+    }, // Make sure to add a comma before the next section
+    plugins: [
+        new htmlWebpackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html" // The last line does not need a comma
+        })
+    ] // The last section does not need a comma
 }
