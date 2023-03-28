1b1bfff9df1a3e58baaad0add03da75cca41f170
michihodges <michihodges@gmail.com>
2023-03-28 11:06:38 +0200

    feat: Project folder

--- src/server/index.js
+++ src/server/index.js
@@ -4,14 +4,12 @@ const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
 
-// app.use(express.static('src/client')) // Old
-app.use(express.static('dist')) // New
+app.use(express.static('src/client'))
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('/client/views/index.html', { root: __dirname + '/..' }) // Old
-    res.sendFile('dist/index.html') // New
+    res.sendFile('/client/views/index.html', { root: __dirname + '/..' })
 })
 
 // designates what port the app will listen to for incoming requests
